fd9ac92d929c7c1a155949b1538a77c73519ddfb
awa <awasoumbane281@gmail.com>
2023-01-11 09:32:43 +0000

    dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+import { ModificationComponent } from './modification/modification.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    NavbarComponent,
+    SidebarComponent,
+  
+    ModificationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import dashboard from '../histo.json';
+import tableau from '../tableauhis.json';
+import profils from '../profil.json';
+
+
+
+interface historique{
+  Heure: string;
+ temperature: string;
+ humidite: string;
+ img:string;
+}
+interface tab{
+  humidite: string;
+ temperature: string;
+ 
+}
+interface DONNE{
+  NOM: string;
+PRENOM: string;
+ 
+}
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent   {
+ 
+
+
+
+imageSrc = 'assets/fan.png';
+messageText = '';
+imageButtons = [ {src: 'assets/fan.png', name: 'OFF'}, 
+{src: 'assets/fan2.gif', name: 'ON'}]
+
+histo:historique[]=dashboard;
+tableauhis:tab[]=tableau;
+profil:DONNE[]=profils;
+ngOnInit():void{
+ 
+}
+onClick(imageNameObject: { src: string; name: string; }) {
+  this.imageSrc = imageNameObject.src;
+  this.messageText = imageNameObject.name;
+}
 
 }
