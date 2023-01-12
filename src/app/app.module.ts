f021c38cf0f9983a45a62d7e328258d3a075ce11
gonarhane <gonar90@gmail.com>
2023-01-12 18:14:44 +0000

    front inscription connexion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InscriptionComponent } from './inscription/inscription.component';
+import { ConnectionComponent } from './connection/connection.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    InscriptionComponent
+    InscriptionComponent,
+    ConnectionComponent,
+    
+    
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {  FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+/* import { AbstractControl, ValidatorFn } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { User } from '../models/User'; */
 
 @Component({
   selector: 'app-inscription',
   templateUrl: './inscription.component.html',
   styleUrls: ['./inscription.component.css']
 })
-export class InscriptionComponent {
+export class InscriptionComponent  {
 
+ 
+  constructor(private builder: FormBuilder){
+    this.profileForm.valueChanges.subscribe((data) => {
+      console.log(data);
+      console.log(this.profileForm.controls.firstName.errors);
+      
+    })
+  }
+
+  profileForm = this.builder.group({
+    firstName: ['', [Validators.required, this.noWhitespaceValidator]],
+    lastName: ['', [Validators.required, this.noWhitespaceValidator]],
+    Email: ['', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+    Role: new FormControl('', Validators.required),
+    Password: new FormControl('', Validators.required),
+    Confirm: new FormControl('', Validators.required),
+  }, {
+    validators: this.MustMatch('Password', 'Confirm')
+  });
+
+    //ici j'exporte la class MushMatch pour la gestion de mes mots de passes
+ MustMatch(controlName: string, matchingControlName: string) {
+  return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+          //renvoie si un autre validateur a déjà trouvé une erreur sur le matchingControl
+          return;
+      }
+
+      //définir une erreur sur matchingControl si la validation échoue
+      if (control.value !== matchingControl.value) {
+          matchingControl.setErrors({ mustMatch: true });
+      } else {
+          matchingControl.setErrors(null);
+      }
+  }
 }
+ noWhitespaceValidator(control: FormControl) {
+    const isWhitespace = (control.value || '').trim().length === 0;
+    const isValid = !isWhitespace;
+    return isValid ? null : { 'whitespace': true };
+}
+
+  }
+  
+  
+  
+ 
+    
+
+    
+
+  
+
+    
+
+
+
+ 
+  
+
